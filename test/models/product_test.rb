9ce694be2031cb7e83869d540ceab008d9443aa5
Gryaz <troller023@gmail.com>
2015-05-25 14:03:33 +0400

    nauchilsya delat' blochnie testi i ispol'zovat' stendi

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	fixtures :products
+test "Свойства товара не должны оставаться пустыми" do
+  	product = Product.new
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:descriptions].any?
+  	assert product.errors[:image_url].any?
+  	assert product.errors[:price].any?
 end
